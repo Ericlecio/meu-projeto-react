47ba7e12546145836eeb0efebc9cb70e462069ea
Ericlecio <ericleciojr14@gmail.com>
2023-07-28 14:07:27 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 // import logo from './logo.svg';
-import "./App.css";
-import Condicional from "./components/Condicional";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Contato from "./pages/Contato";
+import Empresa from "./pages/Empresa";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Renderização condicional</h1>
-      <Condicional />
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <link to="/">Home</link>
+        </li>
+        <li>
+          <link to="/empresa">empresa</link>
+        </li>
+        <li>
+          <link to="/contato">Contato</link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/empresa" element={<Empresa />} />
+        <Route exact path="/contato" element={<Contato />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
